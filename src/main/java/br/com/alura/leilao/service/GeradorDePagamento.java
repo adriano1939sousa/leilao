a7cad96376758f6121b0de0c51a75ab69eee4336
Adriano Sousa <andrade.aas@gmail.com>
2021-09-16 14:47:14 -0300

    final

--- src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -1,5 +1,7 @@
 package br.com.alura.leilao.service;
 
+import java.time.Clock;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +15,29 @@ import br.com.alura.leilao.model.Pagamento;
 public class GeradorDePagamento {
 
 	private PagamentoDao pagamentoDAO;
+	private Clock clock;
 
 	@Autowired
-	public GeradorDePagamento(PagamentoDao pagamentoDAO) {
+	public GeradorDePagamento(PagamentoDao pagamentoDAO, Clock clock) {
 		this.pagamentoDAO = pagamentoDAO;
+		this.clock = clock;
 	}
 
 	public void gerarPagamento(Lance lanceVencedor) {
-		LocalDate vencimento = LocalDate.now().plusDays(1);
-		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
+		LocalDate vencimento = LocalDate.now(clock).plusDays(1);
+		Pagamento pagamento = new Pagamento(lanceVencedor, proximoDiaUtil(vencimento));
 		this.pagamentoDAO.salvar(pagamento);
+	}
+
+	private LocalDate proximoDiaUtil(LocalDate vencimento) {
+		DayOfWeek diaDaSemana = vencimento.getDayOfWeek();
+		if(diaDaSemana.equals(DayOfWeek.SATURDAY)) {
+			return vencimento.plusDays(2);
+		}else if(diaDaSemana.equals(DayOfWeek.SUNDAY)) {
+			return vencimento.plusDays(1);
+		}
+
+		return vencimento;
 	}
 
 }
--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -3,8 +3,12 @@ package br.com.alura.leilao.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,10 +37,13 @@ class GeradorDePagamentoTest {
 	@Captor
 	private ArgumentCaptor<Pagamento> captor;
 	
+	@Mock
+	private Clock clock;
+	
 	@BeforeEach
 	public void beforeach() {
 		MockitoAnnotations.initMocks(this);
-		this.gerador = new GeradorDePagamento(pagamentoDao);
+		this.gerador = new GeradorDePagamento(pagamentoDao, clock);
 		
 	}
 	
@@ -44,13 +51,24 @@ class GeradorDePagamentoTest {
 	void gerarPagamentoTest() {
 		Leilao leilao = leilao();
 		Lance vencedor = leilao.getLanceVencedor();
+		
+		LocalDate data = LocalDate.of(2021, 12, 11);
+		
+//		Mockito.when(data.getDayOfWeek().SATURDAY).thenReturn(data.plusDays(2).getDayOfWeek().MONDAY);
+//		Mockito.when(data.getDayOfWeek().SUNDAY).thenReturn(data.plusDays(1).getDayOfWeek().MONDAY);
+		
+		Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+		
+		Mockito.when(clock.instant()).thenReturn(instant);
+		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+		
 		gerador.gerarPagamento(vencedor);
 		
 		Mockito.verify(pagamentoDao).salvar(captor.capture());
 		
 		Pagamento pagamento = captor.getValue();
 		
-		Assert.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+		Assert.assertEquals(LocalDate.now(clock).plusDays(1), pagamento.getVencimento());
 		Assert.assertEquals(vencedor.getValor(), pagamento.getValor());
 		Assert.assertFalse(pagamento.getPago());
 		Assert.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
